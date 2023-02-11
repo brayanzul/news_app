d14b5601426b8a21ecd38e7972b7107bb505baab
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-02-11 12:05:05 -0500

    Add News App Project and work today: Category Model, News Service, Tab1 Page, Tabs2 Page

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -5,7 +5,7 @@ import 'package:provider/provider.dart';
 
 
 class Tab1Page extends StatefulWidget {
-
+  const Tab1Page({super.key});
   @override
   State<Tab1Page> createState() => _Tab1PageState();
 }
@@ -17,7 +17,7 @@ class _Tab1PageState extends State<Tab1Page> with AutomaticKeepAliveClientMixin 
     final headlines = Provider.of<NewsService>(context).headlines;
 
     return Scaffold(
-      body: (headlines.length == 0)
+      body: (headlines.isEmpty)
        ? const Center(child: CircularProgressIndicator())
        :ListaNoticias( headlines )
    );
--- lib/src/pages/tabs2_page.dart
+++ lib/src/pages/tabs2_page.dart
@@ -1,14 +1,83 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/src/models/category_model.dart';
+import 'package:news_app/src/services/news_service.dart';
+import 'package:provider/provider.dart';
 
 
 class Tab2Page extends StatelessWidget {
   const Tab2Page({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
-   );
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            
+            Expanded(child: _ListaCategorias())
+
+          ],
+        )
+       ),
+    );
+  }
+}
+
+class _ListaCategorias extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final categories = Provider.of<NewsService>(context).categories;
+
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index) {
+
+        final cName = categories[index].name;
+
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              _CategoryButton(categories[index]),
+              const SizedBox( height: 5 ),
+              Text( '${ cName[0].toUpperCase() }${ cName.substring(1) }' )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+
+  final CategoryModel categoria;
+
+  const _CategoryButton(this.categoria);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        print('${ categoria.name }');
+      },
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: const EdgeInsets.symmetric( horizontal: 10 ),
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white
+        ),
+        child: Icon(
+          categoria.icon,
+          color: Colors.black54,
+        ),
+      ),
+    );
   }
 }
